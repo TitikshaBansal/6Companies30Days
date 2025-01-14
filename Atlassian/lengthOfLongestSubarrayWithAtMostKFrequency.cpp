eb04b4ac2874c7fb11b7cb9440581aa2a81d5d7d
Titiksha Bansal <129615682+TitikshaBansal@users.noreply.github.com>
2025-01-14 15:45:48 +0530

    Create lengthOfLongestSubarrayWithAtMostKFrequency.cpp

---
+++ Atlassian/lengthOfLongestSubarrayWithAtMostKFrequency.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubarrayLength(vector<int>& nums, int k) {
+        int s = 0, i = 0;
+        map<int,int> m;
+        int len = 0;
+        for(; i<nums.size(); i++){
+            if(++m[nums[i]] > k){
+                len = max(len, i-s);
+                while(m[nums[i]] >k) m[nums[s++]]--;
+            }
+        }
+        len= max(len, i-s);
+        return len;
+    }
+};
