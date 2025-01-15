f3757a92de1e4ecf846a9a4b4579f659f4292696
Titiksha Bansal <129615682+TitikshaBansal@users.noreply.github.com>
2025-01-15 19:54:35 +0530

    Create findTheDistanceValueBetweenTwoArrays.cpp

---
+++ Atlassian/findTheDistanceValueBetweenTwoArrays.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
+        int ans = 0;
+        sort(arr2.begin(), arr2.end());
+        for (int num1 : arr1) {
+            auto lower = lower_bound(arr2.begin(), arr2.end(), num1 - d);
+            auto upper = upper_bound(arr2.begin(), arr2.end(), num1 + d);
+            if (lower == upper) ans++;
+        }
+        return ans;
+    }
+};
