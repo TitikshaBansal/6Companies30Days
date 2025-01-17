242389948cb88a56839fa343746e148c270ea8e6
Titiksha Bansal <129615682+TitikshaBansal@users.noreply.github.com>
2025-01-17 15:37:00 +0530

    Create kDiffPairsInAnArray.cpp

---
+++ Walmart/kDiffPairsInAnArray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        if (k < 0) return 0; 
+        unordered_map<int, int> freq;
+        int count = 0;
+        for (int num : nums) freq[num]++;
+        for (auto& [num, frequency] : freq) {
+            if (k == 0) {
+                if (frequency > 1) count++;
+            } 
+            else {
+                if (freq.find(num + k) != freq.end()) count++;
+            }
+        }
+        return count;
+    }
+};
