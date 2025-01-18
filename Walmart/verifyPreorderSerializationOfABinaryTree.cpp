76a4621a021b923468d4bb77b0d7b827c8ba3788
Titiksha Bansal <129615682+TitikshaBansal@users.noreply.github.com>
2025-01-18 19:55:11 +0530

    Create verifyPreorderSerializationOfABinaryTree.cpp

---
+++ Walmart/verifyPreorderSerializationOfABinaryTree.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValidSerialization(string preorder) {
+        int slots = 1; 
+        vector<string> nodes;
+        stringstream ss(preorder);
+        string node;
+
+        while (getline(ss, node, ',')) {
+            nodes.push_back(node);
+        }
+
+        for (const string& node : nodes) {
+            slots--; 
+            if (slots < 0) return false;
+            if (node != "#") slots += 2;
+        }
+
+        return slots == 0;
+    }
+};
