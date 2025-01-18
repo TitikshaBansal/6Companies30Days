ee27e198f843c5b16ff010ba1a54041d5f945589
Titiksha Bansal <129615682+TitikshaBansal@users.noreply.github.com>
2025-01-18 19:21:47 +0530

    Create friendsOfAppropriateAges.cpp

---
+++ Walmart/friendsOfAppropriateAges.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numFriendRequests(vector<int>& ages) {
+        int ans = 0;
+        sort(ages.begin(), ages.end()); 
+        for (int i = 0; i < ages.size(); ++i) {
+            int start = upper_bound(ages.begin(), ages.end(), 0.5 * ages[i] + 7) - ages.begin();
+            int end = upper_bound(ages.begin(), ages.end(), ages[i]) - ages.begin();
+            if (end > start + 1) { 
+                ans += end - start - 1; 
+            }
+        }
+        return ans;
+    }
+};
